b00917dea404d55b5873db128473026fb08c1a61
Allan LeSage <allanlesage@gmail.com>
2011-04-13 00:02:22 -0500

    Modified for 1.2 settings DATABASE syntax, else MySQL req local.

--- local_settings_example.py
+++ local_settings_example.py
@@ -8,12 +8,16 @@
 # name with "EXTRA_".
 
 # Use a local SQLite database.
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = '/tmp/djangodb'
-DATABASE_USER = ''
-DATABASE_PASSWORD = ''
-DATABASE_HOST = ''
-DATABASE_PORT = ''
+DATABASES = {
+    "default": {
+        "ENGINE": 'sqlite3',
+        "NAME": '/tmp/djangodb',
+        "USER": '',
+        "PASSWORD": '',
+        "HOST": '',
+        "PORT": '',
+    }
+}
 
 # Required for Django Debug Toolbar and other things.
 INTERNAL_IPS = ('127.0.0.1',)
