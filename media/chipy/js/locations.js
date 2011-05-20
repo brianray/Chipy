7ddd2d0659eb4dda16fbd66ffe904011b8370afd
Jeremy McMillan <aphor@aphor.net>
2011-05-19 20:05:54 -0500

    Rewrote the loop to add markers from points in _geo array. http://code.google.com/apis/maps/documentation/javascript/overlays.html#Markers

--- media/chipy/js/locations.js
+++ media/chipy/js/locations.js
@@ -10,32 +10,19 @@ $(function() {
   }
   var map_canvas = new google.maps.Map(myCanvas, myMapOpts);
 
+  // Add markers to the map_canvas from the _geo array of points
   //
-  // DISABLED: YAHOO MAPS CODE NEEDS TO BE REWRITTEN FOR GOOGLE MAPS API 3
-  //
-  if(map_canvas && false) {
+  if(map_canvas) {
 	// add location markers from the _geo tuple to the map
+	var markers = new Array();
 	for(var i = 0; i < _geo.length; ++i) {
 		var g = _geo[i];
-		var myLatLng = new google.maps.LatLng(g[0], g[1]);
-		var myMarker = new YMarker(yPoint);
-		// Create some content to go inside the SmartWindow
-		var myMarkerContent = g[2];
-		// When the marker is clicked, show the SmartWindow
-		YEvent.Capture(myMarker, EventsList.MouseClick, function() {
-			myMarker.openSmartWindow(myMarkerContent); 
-		});
-		// Put the marker on the map
-		map.addOverlay(myMarker);
-		var pageLink = document.getElementById('loc_' + i);
-		if(pageLink) {
-			pageLink.onclick = function() {
-				var geoIndex = parseInt(this.id.replace('loc_', ''), 10);
-				var g = _geo[geoIndex];
-				var yPoint = new YGeoPoint(g[0], g[1]);
-				map.drawZoomAndCenter(yPoint, 12);
-				};
-			}
+		var markerLatLng = new google.maps.LatLng(g[0], g[1]);
+		var markers[i] = new google.maps.Marker({
+												position: markerLatLng,
+												map: map_canvas,
+												title: g[2]
+												});
 		}
 	}
 });
