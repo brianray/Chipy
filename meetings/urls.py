760eb7438c3917108a96c2201b047a0e3f946a77
Brian Ray <bray@li54-25.members.linode.com>
2011-06-02 17:24:08 +0000

    removing Chipy from import path because it is likely the project will not be sub-directory named Chipy

--- meetings/urls.py
+++ meetings/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
     (r'^past/$','django.views.generic.simple.direct_to_template', {'template': 'meetings/past_index.html'}, 'past_meetings'),
     (r'^next/$','django.views.generic.simple.direct_to_template', {'template': 'meetings/next_index.html'}, 'next_meeting'),
     url(r'^embed_video/(?P<id>\d+)/$', embed_video, {},'embed_video'),
-    (r'^rsvp_update$', 'Chipy.meetings.views.rsvp_update'),
+    (r'^rsvp_update$', 'meetings.views.rsvp_update'),
 )
