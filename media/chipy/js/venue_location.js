fb754069a892f32210b6b8de1003d9c9aa406350
Jeremy McMillan <aphor@aphor.net>
2011-07-11 22:43:00 -0500

    more maps JS

--- media/chipy/js/venue_location.js
+++ media/chipy/js/venue_location.js
@@ -1,7 +1,6 @@
 var geocoder = new google.maps.Geocoder();
 
-function drawMapThumb(mapdiv,address,label) {
-  var canvas = mapdiv;
+function drawMapThumb(mapdiv,venueLatLng,label) {
   var mapOpts = {
 	  zoom: 12,
       draggable: false,
@@ -12,8 +11,14 @@ function drawMapThumb(mapdiv,address,label) {
       scrollWheel: false,
       streetViewControl: false
   };
-    
-  var mapCanvas = new google.maps.Map(canvas, mapOpts);
+  
+  // make the map pop
+  // This belongs in CSS usually, but a failed map
+  // degrades gracefully if you change style/class in JS.
+  mapdiv.style.float = 'right';
+  mapdiv.style.height = '80px';
+  mapdiv.style.width = '80px';
+  var mapCanvas = new google.maps.Map(mapdiv, mapOpts);
   var markerOpts = {
       map: mapCanvas,
       position: venueLatLng,
@@ -22,11 +27,12 @@ function drawMapThumb(mapdiv,address,label) {
   var map_marker = new google.maps.Marker(markerOpts);
 };
 
-function geocodeCatcher(results,status) {
-  // puke if status is not happiness
-  // otherwise call drawMapThumb
-}
-
 function mapVenue(mapdiv,address,label) {
-  geocoder.Geocode(stuff);
+  geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            // do the drawmapThumb() call to make sparkles
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+      });
 }
