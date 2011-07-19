f0588e4b03dbee89e2580f99fd90f3ede37ae263
Jeremy McMillan <aphor@aphor.net>
2011-07-18 21:18:43 -0500

    make the meeting block map a little bit bigger

--- media/chipy/js/venue_location.js
+++ media/chipy/js/venue_location.js
@@ -2,22 +2,23 @@ var geocoder = new google.maps.Geocoder();
 
 function drawMapThumb(mapdiv,venueLatLng,label) {
   var mapOpts = {
-	  zoom: 12,
-      draggable: false,
-	  center: venueLatLng,
-	  mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeControl: false,
-      scaleControl: false,
-      scrollWheel: false,
-      streetViewControl: false
+	zoom: 12,
+	draggable: false,
+	center: venueLatLng,
+	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	disableDefaultUI: 1,
+	mapTypeControl: false,
+	scaleControl: false,
+	scrollWheel: false,
+	streetViewControl: false
   };
   
   // make the map pop
   // This belongs in CSS usually, but a failed map
   // degrades gracefully if you change style/class in JS.
   mapdiv.style.float = 'right';
-  mapdiv.style.height = '80px';
-  mapdiv.style.width = '80px';
+  mapdiv.style.height = '120px';
+  mapdiv.style.width = '120px';
   var mapCanvas = new google.maps.Map(mapdiv, mapOpts);
   var markerOpts = {
       map: mapCanvas,
