73a1dde8bb01395076054908cc9daed8db87a397
Allan LeSage <allanlesage@gmail.com>
2011-08-05 13:00:32 -0500

    Removed JS map-hiding for no latLng, template hides instead; enlarged map; some JS-includes.

--- media/chipy/js/venue_location.js
+++ media/chipy/js/venue_location.js
@@ -14,11 +14,6 @@ function drawMapThumb(mapdiv,venueLatLng,label) {
   };
   
   // make the map pop
-  // This belongs in CSS usually, but a failed map
-  // degrades gracefully if you change style/class in JS.
-  mapdiv.style.float = 'right';
-  mapdiv.style.height = '120px';
-  mapdiv.style.width = '120px';
   var mapCanvas = new google.maps.Map(mapdiv, mapOpts);
   var markerOpts = {
       map: mapCanvas,
