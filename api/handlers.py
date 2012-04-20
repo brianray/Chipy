7d955b64aa66fbbb2e00898f1c846e425352a61e
Pat Paasch <ppaasch@gfxi.com>
2012-04-20 17:26:36 -0500

    setting up django-piston environment adding presentors to api

--- api/handlers.py
+++ api/handlers.py
@@ -1,14 +1,28 @@
 from piston.handler import BaseHandler
-from meetings.models import Meeting
+from meetings.models import Meeting, Presentor
 
 class MeetingHandler(BaseHandler):
   allowed_methods = ('GET',)
   model = Meeting
+  fields = ('when', 'which', ('venue', ('name',)), ('topic_set', ('title', 'description', 'length', 'start_time', ('presentor_set', ('name',)))))
 
   def read(self, request, meeting_id=None):
-    print request
     base = Meeting.objects
     if meeting_id:
-      return base.get(pk=meeting_id)
+      meeting_json = []
+      meeting = base.get(pk=meeting_id)
+      meeting_json.append({'meeting': meeting, 'topics': meeting.topic_set.all()})
+      return meeting_json
     else:
+      return Meeting.objects.all()
+
+class PresentorHandler(BaseHandler):
+  allowed_methods = ('GET',)
+  model = Presentor
+  fields = ('name', 'email', 'phone', ('topic_set', ('title','description', 'length', 'start_time')))
+
+  def read(self, request, presentor_id=None):
+      base = Presentor.objects
+      if presentor_id:
+        return base.get(pk=presentor_id)
       return base.all()
--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from api.handlers import MeetingHandler
+from api.handlers import MeetingHandler, PresentorHandler
 
 meeting_handler = Resource(MeetingHandler)
+presentor_handler = Resource(PresentorHandler)
 
 urlpatterns = patterns('', 
-    url(r'^meeting/(?P<meeting_id>[^/]+)/', meeting_handler),
+    url(r'^meetings\.(?P<emitter_format>.+)', meeting_handler),
+    url(r'^presentors\.(?P<emitter_format>.+)', presentor_handler),
     )
 
 
